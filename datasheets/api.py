526e37fd9dda3dfe2687eb064a07d27689946b3c
Thomas Swann <thomassw@kainos.com>
2022-02-12 22:55:52 +0000

    Add datasheet creation

--- datasheets/api.py
+++ datasheets/api.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
+from datasheets.models.datasheet import Datasheet
 
 app = FastAPI()
+
 
 @app.get("/")
 def root():
     return {"message": "testing"}
 
+
 @app.get("/datasheets")
 def datasheets():
     return []
+
+
+@app.post("/datasheets")
+def create_datasheet(datasheet: Datasheet):
+    return {"data": datasheet.json()}
--- test/test_api.py
+++ test/test_api.py
@@ -1,15 +1,24 @@
-
-from src.api import app
+from datasheets.api import app
+from datasheets.models.datasheet import Datasheet
 from fastapi.testclient import TestClient
 
 client = TestClient(app)
 
+
 def test_root():
     resp = client.get("/")
     assert resp.status_code == 200
-    assert resp.json() == { "message":"testing"}
+    assert resp.json() == {"message": "testing"}
+
 
 def test_datasheets():
     resp = client.get("/datasheets")
     assert resp.status_code == 200
     assert resp.json() == []
+
+
+def test_create_datasheet():
+    data = Datasheet(name="test", description="testing")
+    resp = client.post("/datasheets", data=data.json())
+    assert resp.status_code == 200
+    assert resp.json() == { "data": data.json() }
